c6c77c799cf8b18fb301dc33cd33eaa1dc2aa6bf
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2020-12-30 03:08:30 +0900

    [solved] 20.12.30

---
+++ 02.배열/배열 초기화 방법.py
@@ -0,0 +1,20 @@
+<배열 초기화>
+
+int a[21];
+int b[21][21];
+
+// 1. memset -> 비추
+memset(a, 0, sizeof a);
+memset(b, 0, sizeof b);
+
+// 2. for
+for(int i=0; i<21; i++)
+    a[i]=0;
+for(int i=0; i<21; i++)
+    for(int j=0; j<21; j++)
+        b[i][j] = 0;
+
+// 3. fill -> 제일 추천
+fill(a, a+21, 0);
+for(int i=0; i<21; i++)
+    fill(b[i], b[[i]+21, 0);
