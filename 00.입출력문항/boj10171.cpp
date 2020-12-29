7d806f19bb871fa14a58e72ac006e9019299a44e
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2020-12-29 17:08:32 +0900

    [solved]20.12.29

---
+++ 00.입출력문항/boj10171.cpp
@@ -0,0 +1,17 @@
+/*
+boj10171.cpp
+2020.12.29 정설희
+고양이
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "\\    /\\" << "\n" << " )  ( ')" << "\n" << "(  /  )" << "\n" << " \\(__)|";
+
+}
---
+++ 00.입출력문항/boj2742.cpp
@@ -0,0 +1,20 @@
+/*
+boj2742.cpp
+2020.12.29 정설희
+기찍N
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n = 0;
+	cin >> n;
+	for (int i = n; i > 0; i--) {
+		cout << i << "\n" ;
+	}
+}
