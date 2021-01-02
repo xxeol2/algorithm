9c92c985a4193313203ffc02dd4baace33c14275
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-01-02 20:16:36 +0900

    [solved] 21.01.02

--- 04.그래프(DFS,BFS)/boj1260.cpp
+++ 04.그래프(DFS,BFS)/boj1260.cpp
@@ -1,60 +1,55 @@
 /*
 boj1260.cpp
-2020.12.30 정설희
+2021.01.02 정설희
 DFS와 BFS
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> Graph[1001];
-queue<int> q;
-int state[1001];
-void DFS(vector<int> Graph[], int v) {
-    cout << v << " ";
-    sort(Graph[v].begin(), Graph[v].end());
-    for (auto e : Graph[v]) {
-        if (state[e] == 0) {
-            state[e] = 1;
-            DFS(Graph, e);
-        }
-    }
+vector<int> graph[1001];
+int visited[1001];
+
+void DFS(int v) {
+	visited[v] = 1;
+	cout << v << " ";
+	for (auto e : graph[v]) {
+		if (visited[e] == 0)
+			DFS(e);
+	}
 }
 
-
-void BFS(vector<int> Graph[], int v) {
-    cout << v << " ";
-    sort(Graph[v].begin(), Graph[v].end());
-    for (auto e : Graph[v]) {
-        q.push(e);
-    }
-
-    while (!q.empty()) {
-        int e = q.front();
-        q.pop();
-        if (state[e] == 0) {
-            state[e] = 1;
-            BFS(Graph, e);
-        }
-    }
+void BFS(int v) {
+	queue<int> Q;
+	visited[v] = 1;
+	Q.push(v);
+	while (!Q.empty()) {
+		v = Q.front();
+		Q.pop();
+		cout << v << " ";
+		for (auto e : graph[v]) {
+			if (!visited[e]) {
+				visited[e] = 1;
+				Q.push(e);
+			}
+		}
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m, v;
-    cin >> n >> m >> v;
+	int n, m, v;
+	cin >> n >> m >> v;
 
-    for (int i = 0; i < m; i++) {
-        int tmp1, tmp2;
-        cin >> tmp1 >> tmp2;
-        Graph[tmp1].push_back(tmp2);
-        Graph[tmp2].push_back(tmp1);
-    }
-    state[v] = 1;
-    DFS(Graph, v);
-    cout << "\n";
-    for (int i = 0; i < 1001; i++)
-        state[i] = 0;
-    state[v] = 1;
-    BFS(Graph, v);
+	for (int i = 0; i < m; i++) {
+		int c1, c2;
+		cin >> c1 >> c2;
+		graph[c1].push_back(c2);
+		graph[c2].push_back(c1);
+	}
+	for (int i = 0; i < 1001; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+	DFS(v);
+	cout << "\n";
+	memset(visited, 0, sizeof(visited));
+	BFS(v);
 }
